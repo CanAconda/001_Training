c304a85bcbbcb814e26addb13f9c300c23770ab6
David Penfold <david.penfold@gmx.de>
2021-04-24 21:01:51 +0200

    First excercise in python boot camp

---
+++ 01_BootCamp/DiceSimulator.py
@@ -0,0 +1,19 @@
+# The Dice Simulator
+
+# Features: 
+# -Random Number 
+# -ASCII Graphic output
+# -user input
+
+
+# [---------]  11 ->
+# [         ]   
+# [         ]   | 5
+# [         ]   v
+# [---------]
+
+
+for i in range(5):
+    print("\n")
+    for j in range (10):
+        print("-", end="")
