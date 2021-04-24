1277d548936e7f0755a73bedc6ada3de20ef4357
David Penfold <david.penfold@gmx.de>
2021-04-24 21:13:11 +0200

    5 lines of graphical output included

--- 01_BootCamp/DiceSimulator.py
+++ 01_BootCamp/DiceSimulator.py
@@ -12,8 +12,16 @@
 # [         ]   v
 # [---------]
 
+FstSymbol = "["
+LstSymbol ="]"
+LineSymbol ="-"
 
 for i in range(5):
     print("\n")
+    if (i>1 and i<5):
+        Printsymbol = "+"
+    else:
+        Printsymbol = LineSymbol
+    
     for j in range (10):
-        print("-", end="")
+        print(Printsymbol, end="")
