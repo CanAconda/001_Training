22eef4dfea27bb21b79295b68a0a20dbddebfdac
David Penfold <david.penfold@gmx.de>
2021-04-25 11:20:05 +0200

    Dice layout ready - feature 02 completed and ready for mearge into develop branch

--- 01_BootCamp/DiceSimulator.py
+++ 01_BootCamp/DiceSimulator.py
@@ -5,16 +5,16 @@
 # -ASCII Graphic output
 # -user input
 
-
+# 012       11
 # [---------]  11 ->
 # [ 0     0 ]   
 # [    0    ]   | 5
 # [ 0     0 ]   v
 # [---------]
-
+#middle 5
 
 #Create Dice Layout in matrix
-
+num_dice = 5
 num_rows = 5
 num_cols = 11
 matrix = [[" " for m in range(num_cols)] for n in range(num_rows)]
@@ -25,7 +25,22 @@ for i in range(5):
 for j in range(1,10):
     matrix[0][j] = "-"
     matrix[4][j] = "-"
-print(matrix)
+
+
+# Convert Random number into dice eyes
+
+if (num_dice%2!=0):
+    matrix[2][5] = 0        # Only for the number 1,5,3 (Odd numbers)
+if (num_dice>1):
+    matrix[1][2] = 0        # Only for the number 1,5,3 (Odd numbers)
+    matrix[3][8] = 0        # Only for the number 2, 4, 6 (even numbers)
+if (num_dice>3):
+    matrix[1][8]=0
+    matrix[3][2]=0
+if (num_dice==6):
+    matrix[2][2]=0
+    matrix[2][8]=0
+    
 
 # Print matrix in correct oder
 
@@ -33,6 +48,4 @@ for p in range(num_rows):
     print("\r")
     for k in range(num_cols):
         print(matrix[p][k], end="")
-
-
 
