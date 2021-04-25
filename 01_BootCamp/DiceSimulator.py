2c57b88cbbaf45e7d43905b736ebdaf3060ed47c
David Penfold <david.penfold@gmx.de>
2021-04-25 10:58:18 +0200

    Dice print layout added

--- 01_BootCamp/DiceSimulator.py
+++ 01_BootCamp/DiceSimulator.py
@@ -13,26 +13,26 @@
 # [---------]
 
 
-FstSymbol = "["
-LstSymbol ="]"
-LineSymbol ="-"
+#Create Dice Layout in matrix
 
 num_rows = 5
 num_cols = 11
-matrix = [[0 for m in range(num_cols)] for n in range(num_rows)]
+matrix = [[" " for m in range(num_cols)] for n in range(num_rows)]
 for i in range(5):
     matrix[i][0] = "["             #Enter row 0-5 and overwirte the first element [0]
     matrix[i][10] = "]"            #Enter row 0-5 and overwrite the last element[cols -1]
+
+for j in range(1,10):
+    matrix[0][j] = "-"
+    matrix[4][j] = "-"
 print(matrix)
 
-# for i in range(5):
-#     print("\n")
-#     if (i>1 and i<5):
-#         Printsymbol = "+"
-#     else:
-#         Printsymbol = LineSymbol
-    
-#     for j in range (10):
-#         matrix = [[i], j]
-#         print(Printsymbol, end="")
+# Print matrix in correct oder
+
+for p in range(num_rows):
+    print("\r")
+    for k in range(num_cols):
+        print(matrix[p][k], end="")
+
+
 
