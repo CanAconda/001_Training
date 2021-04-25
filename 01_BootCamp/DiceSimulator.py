5fa6644a7338b8fca34bdf9798a6c1fd4dc0180c
David Penfold <david.penfold@gmx.de>
2021-04-25 10:44:54 +0200

    Matrix included for Dice layout

--- 01_BootCamp/DiceSimulator.py
+++ 01_BootCamp/DiceSimulator.py
@@ -7,21 +7,32 @@
 
 
 # [---------]  11 ->
-# [         ]   
-# [         ]   | 5
-# [         ]   v
+# [ 0     0 ]   
+# [    0    ]   | 5
+# [ 0     0 ]   v
 # [---------]
+
 
 FstSymbol = "["
 LstSymbol ="]"
 LineSymbol ="-"
 
+num_rows = 5
+num_cols = 11
+matrix = [[0 for m in range(num_cols)] for n in range(num_rows)]
 for i in range(5):
-    print("\n")
-    if (i>1 and i<5):
-        Printsymbol = "+"
-    else:
-        Printsymbol = LineSymbol
+    matrix[i][0] = "["             #Enter row 0-5 and overwirte the first element [0]
+    matrix[i][10] = "]"            #Enter row 0-5 and overwrite the last element[cols -1]
+print(matrix)
+
+# for i in range(5):
+#     print("\n")
+#     if (i>1 and i<5):
+#         Printsymbol = "+"
+#     else:
+#         Printsymbol = LineSymbol
     
-    for j in range (10):
-        print(Printsymbol, end="")
+#     for j in range (10):
+#         matrix = [[i], j]
+#         print(Printsymbol, end="")
+
